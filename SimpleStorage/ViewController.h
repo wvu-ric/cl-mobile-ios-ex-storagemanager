3ff4a664b3db2a5f4a93955fe5fa4939f8b1661e
rickykirkendall <rickykirkendall@flocoapps.com>
2016-04-12 15:56:28 -0400

    Initial Commit

---
+++ SimpleStorage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleStorage
+//
+//  Created by Ricky Kirkendall on 4/12/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleStorage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleStorage
+//
+//  Created by Ricky Kirkendall on 4/12/16.
+//  Copyright © 2016 CodeLab. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
